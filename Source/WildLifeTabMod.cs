6c4bf2ce952f53e0297cc58b08f1f6d0d4cc7030
- <krypt-lynx@yandex.ru>
2020-12-21 01:23:29 +0200

    init

---
+++ Source/WildLifeTabMod.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using RWLayout.alpha2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace WildtabAlt
+{
+    public class WildLifeTabMod : CMod
+    {
+        public static string PackageIdOfMine = null;
+        //public static Settings Settings { get; private set; }
+        public static string CommitInfo = null;
+
+        public WildLifeTabMod(ModContentPack content) : base(content)
+        {
+            ReadModInfo(content);
+
+            Harmony harmony = new Harmony(PackageIdOfMine);
+        }
+
+        private static void ReadModInfo(ModContentPack content)
+        {
+            PackageIdOfMine = content.PackageId;
+
+            var name = Assembly.GetExecutingAssembly().GetName().Name;
+
+            try
+            {
+                using (Stream stream = Assembly.GetExecutingAssembly()
+                        .GetManifestResourceStream(name + ".git.txt"))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    CommitInfo = reader.ReadToEnd()?.TrimEndNewlines();
+                }
+            }
+            catch
+            {
+                CommitInfo = null;
+            }
+        }
+
+    }
+}
