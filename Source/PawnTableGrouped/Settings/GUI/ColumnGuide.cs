346678c6f9cdbce6c9fcfb440e60ff3f6fbf7bcc
Unknown <krypt-lynx@yandex.ru>
2021-01-02 23:24:36 +0200

    quick select fixes

---
+++ Source/PawnTableGrouped/Settings/GUI/ColumnGuide.cs
@@ -0,0 +1,64 @@
+﻿using Cassowary;
+using RWLayout.alpha2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawnTableGrouped
+{
+    public class ColumnGuide : CLayoutGuide
+    {
+        public ColumnGuide(int columnsCount)
+        {
+            columns_ = new Anchor[columnsCount];
+            for (int i = 0; i < columnsCount; i++)
+            {
+                columns_[i] = new Anchor();
+            }
+        }
+
+        private Anchor[] columns_;
+
+        private ClVariable[] columnsCached = null;
+        public ClVariable[] columns
+        {
+            get
+            {
+                if (columnsCached == null)
+                {
+                    columnsCached = columns_.Select((c, i) => Parent.GetVariable(ref columns_[i], $"column{i}")).ToArray();
+                }
+                return columnsCached;
+            }
+        }
+
+        public void UpdateColumnWidth(int index, double width)
+        {
+            Parent.UpdateStayConstrait(ref columns_[index], width);
+        }
+
+        public override void AddImpliedConstraints()
+        {
+            for (int i = 0; i < columns_.Length; i++)
+            {
+                Parent.CreateConstraintIfNeeded(ref columns_[i], () => new ClStayConstraint(columns[i]));
+            }
+        }
+
+        public override void RemoveImpliedConstraints()
+        {
+            for (int i = 0; i < columns_.Length; i++)
+            {
+                Parent.RemoveVariableIfNeeded(ref columns_[i]);
+            }
+        }
+
+        public override IEnumerable<ClVariable> enumerateAnchors()
+        {
+            return columns;
+        }
+    }
+
+}
---
+++ Source/PawnTableGrouped/Settings/TablesSettingsViewModel.cs
@@ -0,0 +1,144 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace PawnTableGrouped
+{
+    public class CompatibilityInfoDef : Def
+    {
+        public string packageId;
+        public string modName;
+        public List<string> tableNames;
+        public string hint;
+        public string issues;
+        public TableCompatibility compatibility;
+    }
+
+    public enum TableCompatibility
+    {
+        Incompatible,
+        Compatible,
+        Supported,
+    }
+
+    public class TablesSettingsViewModel
+    {
+        public class TableData
+        {
+            public TableCompatibility compatibility;
+            public string defName;
+            public string modName;
+            public string packageId;
+            public bool selected;
+            public string tip;
+            public Action<TableData> OnChanged;
+
+            public void DoChanged()
+            {
+                OnChanged?.Invoke(this);
+            }
+        }
+
+        public List<TableData> Tables;
+        public Dictionary<string, TableData> defToTable;
+
+
+        public TablesSettingsViewModel()
+        {
+            Tables = DefDatabase<PawnTableDef>.AllDefs
+                .Select(def => new TableData
+                {
+                    defName = def.defName,
+                    compatibility = TableCompatibility.Compatible,
+                    selected = Mod.Settings.pawnTablesEnabled.Contains(def.defName),
+                }).ToList();
+
+            defToTable = Tables.ToDictionary(d => d.defName, d => d);
+
+            DetectMods();
+
+        }
+
+        private void DetectMods()
+        {
+            var loadedModIds = LoadedModManager.RunningMods.Select(x => x.PackageId).ToHashSet();
+
+
+            var info = DefDatabase<CompatibilityInfoDef>.AllDefs;
+
+
+            foreach (var compatibility in info)
+            {
+                if (loadedModIds.Contains(compatibility.packageId))
+                {
+                    foreach (var tableName in compatibility.tableNames)
+                    {
+                        TableData data = null;
+                        if (defToTable.TryGetValue(tableName, out data))
+                        {
+                            data.compatibility = compatibility.compatibility;
+                            data.modName = compatibility.modName;
+                            data.tip = compatibility.issues == null
+                                ? compatibility.hint
+                                : $"{compatibility.hint}\n\nIssues: {compatibility.issues}";
+                            data.packageId = compatibility.packageId;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void SetSelected(TableData table, bool value)
+        {
+            table.selected = value;
+            if (value)
+            {
+                Mod.Settings.pawnTablesEnabled.Add(table.defName);
+            }
+            else
+            {
+                Mod.Settings.pawnTablesEnabled.Remove(table.defName);
+            }
+            Mod.DoActiveTablesChanged();
+        }
+
+        public void SelectAllAtLeast(TableCompatibility compatibility)
+        {
+            Mod.Settings.pawnTablesEnabled.Clear();
+
+            foreach (var table in Tables)
+            {
+                if (table.compatibility >= compatibility)
+                {
+                    Mod.Settings.pawnTablesEnabled.Add(table.defName);
+                    table.selected = true;
+                }
+                else
+                {
+                    table.selected = false;
+                }
+                table.DoChanged();
+            }
+
+            Mod.DoActiveTablesChanged();
+        }
+
+        public void SelectNone()
+        {
+            Mod.Settings.pawnTablesEnabled.Clear();
+
+            foreach (var table in Tables)
+            {
+                table.selected = false;
+                table.DoChanged();
+            }
+
+            Mod.DoActiveTablesChanged();
+        }
+    }
+
+}
