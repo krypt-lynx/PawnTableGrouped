aa521c7c5e3781b9fe5d76d15480a77dc42cdcbd
- <krypt-lynx@yandex.ru>
2021-02-12 06:42:41 +0200

    GroupColumnWorker_Gap

---
+++ Source/PawnTableGrouped/GroupColumns/GroupColumnWorker_Gap.cs
@@ -0,0 +1,55 @@
+﻿using RimWorld;
+using RWLayout.alpha2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace PawnTableGrouped
+{
+    public class GroupColumnWorker_Gap : GroupColumnWorker
+    {
+        public override bool CanSetValues()
+        {
+            return false;
+        }
+
+        public override object DefaultValue(IEnumerable<Pawn> pawns)
+        {
+            return null;
+        }
+
+        public override object GetValue(Pawn pawn)
+        {
+            return "Oops";
+        }
+
+        public override bool IsUniform(IEnumerable<Pawn> pawns)
+        {
+            return true;
+        }
+
+        public override bool IsGroupVisible(IEnumerable<Pawn> pawns)
+        {
+            return false;
+        }
+
+        public override bool IsVisible(Pawn pawn)
+        {
+            return false;
+        }
+
+        public override void SetValue(Pawn pawn, object value)
+        {
+
+        }
+
+        public override void DoCell(Rect cellRect, PawnTableGroupColumn column, PawnTable table)
+        {
+
+        }
+    }
+}
