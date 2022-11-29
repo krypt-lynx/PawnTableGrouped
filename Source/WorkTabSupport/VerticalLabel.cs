8a1a69f042f2f11dff23e35d769f356345be2e2f
- <@>
2022-11-29 02:04:12 +0200

    version 2.1.1

--- Source/WorkTabSupport/VerticalLabel.cs
+++ Source/WorkTabSupport/VerticalLabel.cs
@@ -24,7 +24,7 @@ namespace PawnTableGrouped
             // the best idea I have is to draw on (0,0) of current clip area and move label to desized position using gui matrix intead of label coordinates
             // limitations:
             // we need to apply clip effect to transformed label manually, we need to know clip area size for that, but I see no way to obrain it (in this implementation I do not clip right and bottom sides at all, though)
-            // original clip is still applied, and width and height is now inverted. So, original clip have no space to render the label *horizontaly*, the vertical label will not be rendered correctly either.
+            // original clip is still applied, and width and height is now inverted. So, if original clip have no space to render the label *horizontaly*, the vertical label will not be rendered correctly either.
 
             Matrix4x4 matrix = GUI.matrix;
             GUI.matrix = Matrix4x4.identity;
@@ -32,11 +32,11 @@ namespace PawnTableGrouped
             Vector2 unclipped = UnclipVector2(Vector2.zero);
 
             GUI.matrix =
+                matrix *
                 // move origin to (0,0) of current clip; rotate around (0,0)
                 Matrix4x4.TRS(unclipped, Quaternion.Euler(0f, 0f, -90), Vector3.one) *
                 // move (0,0) to label possition
-                Matrix4x4.TRS(new Vector2(-rect.yMax - unclipped.x, rect.xMin - unclipped.y), Quaternion.identity, Vector3.one) *
-                GUI.matrix;
+                Matrix4x4.TRS(new Vector2(-rect.yMax - unclipped.x, rect.xMin - unclipped.y), Quaternion.identity, Vector3.one);
 
             // calculating clipping effects
             var leftClip = Mathf.Min(rect.xMin, 0);
@@ -44,10 +44,11 @@ namespace PawnTableGrouped
 
             // applying clip
             var clip = new Rect(0, -leftClip, rect.height + topClip, rect.width + leftClip);
+
             GUI.BeginClip(clip);
 
             // rendering whatever we want to render
-            var adjusted = new Rect(0, leftClip, rect.height + 1, rect.width); // +1 is to avoid jittering caused by worktab being a little to greedy with label size
+            var adjusted = new Rect(0, leftClip, rect.height + 1, rect.width); // +1 is to avoid jittering caused by worktab being a little too greedy with label size
             Widgets.Label(adjusted, text);
 
             GUI.EndClip();
